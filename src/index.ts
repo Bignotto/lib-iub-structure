404eb6e0199f289fcb77e906f37118acd7dc5a9c
Thiago Bignotto <bignotto@gmail.com>
2021-06-07 11:53:53 -0300

    save commit

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
 import path from "path";
 import fs from "fs/promises";
-import { json2csv, json2csvAsync } from "json-2-csv";
+import { json2csvAsync } from "json-2-csv";
 
 import IubStructure from "./lib/IubStructure";
 
 var iubStruct: IubStructure;
 
 const load = async () => {
-  const file = path.resolve(__dirname, "data", "inputs_data.json");
+  const inputs_file = path.resolve(__dirname, "data", "inputs_data.json");
 
-  iubStruct = await IubStructure.CreateIubStructure(file);
+  iubStruct = await IubStructure.CreateIubStructure(inputs_file);
 
   const struct = iubStruct.getIubStructBySap("I01384");
 
   const csvString = await json2csvAsync(struct, {
     delimiter: {
-      field: ";", // Comma field delimiter
+      field: ";",
     },
   });
 
-  await fs.writeFile("output.csv", csvString);
+  await fs.writeFile("output.csv", csvString, "utf-8");
   return;
 };
 
