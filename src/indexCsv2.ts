d43a8aa5649ad0722869b7140fd1327aba7dc233
Thiago Bignotto <bignotto@gmail.com>
2022-01-11 14:07:46 -0300

    small fixes on filenames

--- src/indexCsv2.ts
+++ src/indexCsv2.ts
@@ -1,51 +1,54 @@
-import path from "path"
-import fs from "fs/promises"
-import { json2csvAsync } from "json-2-csv"
+import path from "path";
+import fs from "fs/promises";
+import { json2csvAsync } from "json-2-csv";
 
-import IubStructure from "./lib/IubStructure"
-import { StructLine } from "lib/IubStructure/models/IStructLine"
+import IubStructure from "./lib/IubStructure";
+import { StructLine } from "lib/IubStructure/models/IStructLine";
 
-import { list } from "./list"
+import { list } from "./products2022";
 
-var iubStruct: IubStructure
+var iubStruct: IubStructure;
 
-type Structures = StructLine & { productCode: string }
+type Structures = StructLine & { productCode: string };
 
 const load = async () => {
-  const inputs_file = path.resolve(__dirname, "data", "roteiros.csv")
-  const prices_file = path.resolve(__dirname, "data", "precos.csv")
+  const inputs_file = path.resolve(__dirname, "data", "roteiros.csv");
+  const prices_file = path.resolve(__dirname, "data", "precos.csv");
 
-  const outputPath = path.resolve(__dirname, "output")
+  const outputPath = path.resolve(__dirname, "output");
 
-  iubStruct = await IubStructure.CreateIubStructureCSV(inputs_file, prices_file)
+  iubStruct = await IubStructure.CreateIubStructureCSV(
+    inputs_file,
+    prices_file
+  );
 
-  let structs: Structures[] = []
+  let structs: Structures[] = [];
 
-  list.forEach(async item => {
-    const struct = iubStruct.getIubStructBySap(item)
+  list.forEach(async (item) => {
+    const struct = iubStruct.getIubStructBySap(item);
 
-    struct.map(line => {
+    struct.map((line) => {
       structs.push({
         ...line,
         productCode: item.toUpperCase(),
-      })
-    })
-  })
+      });
+    });
+  });
 
   const csvString = await json2csvAsync(structs, {
     delimiter: {
       field: ";",
     },
     excelBOM: true,
-  })
+  });
 
-  const outputFile = path.resolve(outputPath, `estruturas.csv`)
-  await fs.writeFile(outputFile, csvString, "utf-8")
+  const outputFile = path.resolve(outputPath, `estruturas2022.csv`);
+  await fs.writeFile(outputFile, csvString, "utf-8");
 
-  return
-}
+  return;
+};
 
-load()
+load();
 
 // [x] recursive function to filter inputs for a given product
 //    [x] by sap code
